25c72a34b556156fbf3b4f29fe9d4d106894ed76
Vitaly <80533049+visurm76@users.noreply.github.com>
2023-01-06 18:20:40 +0300

    Add files via upload
    Foto control

---
+++ json_load.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+
+def read_json_files():
+    """
+    Функиция чтения json файлов и формирует список списков из номера квартала и номера выдела
+    :return: список списков
+    """
+    link = os.listdir('/home/viktor/Общедоступные/json_project/json_files')  # Получает список имен файлов в директории
+    path = os.getcwd()  # Текущая директория
+    lst_data = []
+    for i in sorted(link):
+        # Открываем поочередно файлы и считываем данные
+        with open(path + '/json_files/' + i, "r") as read_file:
+            data = json.load(read_file)
+            # Фильтруем словарь с номером квартала и выдела
+            mass_kv_vid = {k: v for k, v in data.items() if k == 'kv' or k == 'vid'}
+            lst_data.append(list(mass_kv_vid.values()))
+
+    return lst_data
+
+
+
