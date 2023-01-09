0830af70ed042227b2c1f05f615e41414baacb7b
visurm76 <visurm76@gmail.com>
2023-01-09 10:00:13 +0300

    Вывод данных в виде таблицы

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import package
 
 # Путь, где хранится файл базы данных
-path_connect_sqlite = '/home/viktor/Общедоступные/Foto_control/package/gubaha.sqlite'
-
+path_connect_sqlite = '/home/viktor/Общедоступные/Foto_control/gubaha.sqlite'
+print('_____________________________________')
+print('|№ кв|№ выдела|Наличие фото|Таксатор|')
+print('-------------------------------------')
 for foto in package.connect_sqlite(path_connect_sqlite):
     if foto not in package.read_json_files():
-        print('------------------------------')
-        print(f'Нет фото: кв.{foto[0]} выдел {foto[1]}')
+        print('|{:4}| {:7}| {:11}|'.format(foto[0], foto[1], 'Нет'))
 
 for vid in package.read_json_files():
     if vid not in package.connect_sqlite(path_connect_sqlite):
