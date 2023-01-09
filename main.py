255849e074e74d5735a04679358e08000468291f
visurm76 <visurm76@gmail.com>
2023-01-09 10:43:45 +0300

    окончательный вывод данных в виде таблиц

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import package
 
+
+
 # Путь, где хранится файл базы данных
 path_connect_sqlite = '/home/viktor/Общедоступные/Foto_control/gubaha.sqlite'
-print('_____________________________________')
-print('|№ кв|№ выдела|Наличие фото|Таксатор|')
-print('-------------------------------------')
+print('___________________________')
+print('|№ кв|№ выдела|Наличие фото|')
+print('____________________________')
 for foto in package.connect_sqlite(path_connect_sqlite):
     if foto not in package.read_json_files():
-        print('|{:4}| {:7}| {:11}|'.format(foto[0], foto[1], 'Нет'))
-
+        print('|{:4}| {:7}| {:>11}|'.format(foto[0], foto[1], 'Нет'))
 for vid in package.read_json_files():
     if vid not in package.connect_sqlite(path_connect_sqlite):
-        print(f'Фото в непокрытой лесом площади: кв.{vid[0]} выдел {vid[1]} ')
+        print()
+        print('________________________________')
+        print(f'Фото в непокрытой лесом площади')
+        print('________________________________')
+        print('|№ кв|№ выдела|Наличие фото|')
+        print('________________________________')
+        print('|{:4}| {:7}|{:>12}|'.format(vid[0],vid[1], 'Да'))
