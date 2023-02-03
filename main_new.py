2f782e5e5599bdc3a750c8a6d69e5e26f7c5d961
visurm76 <visurm76@gmail.com>
2023-02-03 17:47:55 +0300

    Добавлен вывод в Excel

--- main_new.py
+++ main_new.py
@@ -1,21 +1,9 @@
 import package as pkg
 import openpyxl as op
-import os
 
-NAME_FILE = 'list.xlsx'
+# NAME_FILE = 'list.xlsx'
 FILENAME = 'gubaha_kizel.sqlite'
 TABLENAME = 'gubaha_vydel'
-
-
-def searchFile(filename):
-    """
-    Функиция определения пути расположения файла по его названию
-    """
-
-    for root, dirnames, filenames in os.walk('/home/viktor/'):
-        for file in filenames:
-            if file == filename:
-                return os.path.join(root, file)
 
 
 class PhotoFilter(object):
@@ -25,9 +13,6 @@ class PhotoFilter(object):
 
     def __init__(self, *args):
         self.row = list(args)
-
-    def __repr__(self):
-        return str(self.row)
 
     def fotoFilter(self):
         mass_filter = []
@@ -51,17 +36,30 @@ class SqliteFilter(object):
     def sqliteFilter(self):
         row_name = ','.join(self.row)
         sql = 'SELECT' + ' ' + row_name + ' ' + 'FROM' + ' ' + TABLENAME
-        return pkg.connect_sqlite(searchFile(FILENAME), sql)
+        return pkg.connect_sqlite(pkg.searchFile(FILENAME), sql)
 
 
 d = PhotoFilter('kv', 'vid')
 a = PhotoFilter('kv', 'vid', 'tax_name')
 s = SqliteFilter('kv', 'sknr', 'zk')
 
-lst = []
 
-for foto in s.sqliteFilter():
-    if foto not in d.fotoFilter():
-        lst.append(foto)
-print(lst)
-print(a[0][2])
+def filterFotoVydel():
+    lst = []
+    for foto in s.sqliteFilter():
+        if foto not in d.fotoFilter():
+            lst.append(foto)
+    return lst
+
+
+wb = op.Workbook()
+ws = wb.active
+sheet = wb['Sheet']
+dict_name_row = {'A1': 'Квартал', 'B1': 'Выдел', 'C1': 'Имя таксатора'}
+for key, val in dict_name_row.items():
+    sheet[key] = val
+    sheet[key].value
+
+for row in filterFotoVydel():
+    ws.append(row)
+wb.save('files_ex/' + a[0][2] + '.xlsx')
